60f162a07374402aa3215e540e97a9ac962400cc
VitorSantana113 <vitor@t2s.com.br>
2024-11-11 23:28:46 -0300

    Implemetação da API do Google Maps

--- fatour/home/pedidos/script.js
+++ fatour/home/pedidos/script.js
@@ -15,33 +15,3 @@ window.addEventListener('click', (event) => {
         modal.style.display = 'none';
     }
 });
-
-document.getElementById('search-button').addEventListener('click', function () {
-    const searchValue = document.getElementById('search-bar').value.trim().toLowerCase();
-    const orders = document.querySelectorAll('.order');
-
-    let found = false;
-
-    orders.forEach(order => {
-        const orderCode = order.querySelector('h3, .product-code').textContent.toLowerCase();
-
-        if (orderCode.includes(searchValue)) {
-            order.classList.remove('highlight');
-
-            setTimeout(() => {
-                order.classList.add('highlight');
-            }, 10);
-            setTimeout(() => {
-                order.classList.remove('highlight');
-            }, 3000);
-
-            found = true;
-        } else {
-            order.classList.remove('highlight');
-        }
-    });
-
-    if (!found) {
-        alert('Nenhum pedido encontrado com esse código.');
-    }
-});
